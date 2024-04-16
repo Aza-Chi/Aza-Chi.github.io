b3be439f0b45974c5e2607a8f3ee9197abc4f10d
Chi <azarashibobobo@gmail.com>
2024-04-16 22:17:31 +0100

    updated style.css index.html script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,41 @@
+// Spotlight Mouse
 let root = document.documentElement;
 
 root.addEventListener("mousemove", e => {
   root.style.setProperty('--mouse-x', (e.clientX - 450) + "px");
   root.style.setProperty('--mouse-y', (e.clientY - 440 ) + "px");
 });
+// Spotlight Mouse End 
+
+// Dark Mode / Theme Toggle 
+
+const themeToggle = document.querySelector("#theme-toggle");
+
+themeToggle.addEventListener("click", () => {
+  document.body.classList.contains("light-theme")
+    ? enableDarkMode()
+    : enableLightMode();
+});
+
+function enableDarkMode() {
+  document.body.classList.remove("light-theme");
+  document.body.classList.add("dark-theme");
+  themeToggle.setAttribute("aria-label", "Switch to light theme");
+}
+
+function enableLightMode() {
+  document.body.classList.remove("dark-theme");
+  document.body.classList.add("light-theme");
+  themeToggle.setAttribute("aria-label", "Switch to dark theme");
+}
+
+function setThemePreference() {
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    enableDarkMode();
+    return;
+  }
+  enableLightMode();
+}
+
+document.onload = setThemePreference();
 
