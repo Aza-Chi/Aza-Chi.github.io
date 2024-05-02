2d89a63f1807ed9fbb9552918ff93c8fe51f6d6f
Chi <azarashibobobo@gmail.com>
2024-05-02 23:05:18 +0100

    updated style.css index.html scripts.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,14 @@
-// Spotlight Mouse
+/////////////////// Spotlight Mouse
 let root = document.documentElement;
 
 root.addEventListener("mousemove", e => {
   root.style.setProperty('--mouse-x', (e.clientX - 450) + "px");
   root.style.setProperty('--mouse-y', (e.clientY - 440 ) + "px");
 });
-// Spotlight Mouse End 
+////////////////// Spotlight Mouse End 
 
-// Dark Mode / Theme Toggle 
+
+///////////////// Dark Mode / Theme Toggle 
 
 const themeToggle = document.querySelector("#theme-toggle");
 
@@ -39,3 +40,87 @@ function setThemePreference() {
 
 document.onload = setThemePreference();
 
+/////////////// Dark Mode End 
+
+
+
+/////////////// Highlight Menu Item WIP!!!
+
+const navMenu = document.querySelectorAll("section");
+const navList = document.getElementById("navbar__list");
+const items = ["About", "Education", "Projects", "Section 4"];
+let lastId;
+let last_known_scroll_position = 0;
+let ticking = false;
+
+//Build the nav
+items.forEach((item, i) => {
+  const li = document.createElement("li");
+  const el = document.createElement("a");
+  el.innerText = item;
+  el.classList.add("menu-items");
+  el.setAttribute("id", `menu-${i + 1}`);
+  el.href = `#section${i + 1}`;
+
+  el.addEventListener("click", function(e) {
+    const href = e.target.getAttribute("href"),
+      offsetTop = href === "#" ? 0 : e.target.offsetTop - topMenuHeight + 1;
+    const scrollOptions = {
+      scrollIntoView: true,
+      behavior: "smooth"
+    };
+    e.target.scrollIntoView(scrollOptions);
+    e.preventDefault();
+  });
+
+  navList.appendChild(li);
+  li.appendChild(el);
+});
+
+const topMenu = document.getElementById("navbar__list");
+const topMenuHeight = topMenu.offsetHeight + 1;
+const menuItems = document.querySelectorAll(".menu-items");
+const scrollItems = document.querySelectorAll(".sec");
+
+//Make Nav Active when Clicked and scrolls down to section
+document.addEventListener("click", function(event) {
+  let active = document.querySelector(".active");
+  if (active) {
+    active.classList.remove("active");
+  }
+  if (event.target.classList.contains("menu-items")) {
+    event.target.classList.add("active");
+  }
+});
+
+// Bind to scroll
+window.addEventListener("scroll", function() {
+  // Get container scroll position
+  const container = document.querySelector(".container");
+  let fromTop = window.pageYOffset + topMenuHeight + 40;
+
+  // Get id of current scroll item
+  let cur = [];
+
+  [...scrollItems].map(function(item) {
+    if (item.offsetTop < fromTop) {
+      cur.push(item);
+    }
+  });
+
+  // Get the id of the current element
+  cur = cur[cur.length - 1];
+  let id = cur ? cur.id : "";
+
+  if (lastId !== id) {
+    lastId = id;
+
+    menuItems.forEach(function(elem, index) {
+      elem.classList.remove("active");
+      const filteredItems = [...menuItems].filter(elem => elem.getAttribute("href") === `#${id}`);
+      filteredItems[0].classList.add("active");
+    });
+  }
+});
+
+/////////////// Highlight Menu Item END
