d035e7d7c81e052f445a3c7d3701ffc1f140dea8
Chi <azarashibobobo@gmail.com>
2024-05-03 20:43:24 +0100

    updated style.css index.html script.js

--- scripts/script.js
+++ scripts/script.js
@@ -43,72 +43,65 @@ document.onload = setThemePreference();
 /////////////// Dark Mode End 
 
 
-
+///////////////////
 /////////////// Highlight Menu Item WIP!!!
 
-const navMenu = document.querySelectorAll("section");
 const navList = document.getElementById("navbar__list");
-const items = ["About", "Education", "Projects", "Section 4"];
+// const items = ["About", "Education", "Section 3", "Projects"];
+const items = ["About", "Education", "Projects"];
+const topMenu = document.getElementById("navbar__list");
 let lastId;
-let last_known_scroll_position = 0;
-let ticking = false;
 
 //Build the nav
 items.forEach((item, i) => {
-  const li = document.createElement("li");
   const el = document.createElement("a");
   el.innerText = item;
   el.classList.add("menu-items");
   el.setAttribute("id", `menu-${i + 1}`);
   el.href = `#section${i + 1}`;
+  //navList.appendChild(el);
 
-  el.addEventListener("click", function(e) {
-    const href = e.target.getAttribute("href"),
-      offsetTop = href === "#" ? 0 : e.target.offsetTop - topMenuHeight + 1;
-    const scrollOptions = {
-      scrollIntoView: true,
-      behavior: "smooth"
-    };
-    e.target.scrollIntoView(scrollOptions);
-    e.preventDefault();
-  });
-
-  navList.appendChild(li);
+  const li = document.createElement("li");
+  li.classList.add("menu-list");
   li.appendChild(el);
+  li.setAttribute("id", `${i + 1}`);
+  // Append the list item to the list
+  li.appendChild(el);
+  navList.appendChild(li);
 });
-
-const topMenu = document.getElementById("navbar__list");
-const topMenuHeight = topMenu.offsetHeight + 1;
-const menuItems = document.querySelectorAll(".menu-items");
-const scrollItems = document.querySelectorAll(".sec");
 
 //Make Nav Active when Clicked and scrolls down to section
 document.addEventListener("click", function(event) {
-  let active = document.querySelector(".active");
-  if (active) {
-    active.classList.remove("active");
-  }
-  if (event.target.classList.contains("menu-items")) {
+  let active = document.querySelector(".menu-list.active");
+  if (active) active.classList.remove("active");
+  if (event.target.classList.contains("menu-list")) {
     event.target.classList.add("active");
+    window.location.href = "#section" + event.target.id -500;
   }
 });
+
+const topMenuHeight = topMenu.offsetTop - 100; //Zoom stuff for highlight nav
+const menuItems = document.querySelectorAll(".menu-list");
+const scrollItems = document.querySelectorAll("section");
 
 // Bind to scroll
 window.addEventListener("scroll", function() {
   // Get container scroll position
-  const container = document.querySelector(".container");
-  let fromTop = window.pageYOffset + topMenuHeight + 40;
-
+  const mainHeroHeight = document.querySelector(".main__hero").offsetTop;
+  console.log(mainHeroHeight);
+  let fromTop = window.pageYOffset + topMenuHeight + mainHeroHeight;
   // Get id of current scroll item
   let cur = [];
 
   [...scrollItems].map(function(item) {
+    //debugger;
     if (item.offsetTop < fromTop) {
       cur.push(item);
     }
   });
 
   // Get the id of the current element
+  //debugger;
   cur = cur[cur.length - 1];
   let id = cur ? cur.id : "";
 
@@ -117,10 +110,13 @@ window.addEventListener("scroll", function() {
 
     menuItems.forEach(function(elem, index) {
       elem.classList.remove("active");
-      const filteredItems = [...menuItems].filter(elem => elem.getAttribute("href") === `#${id}`);
+      // Look at the child "a" tags and try to find the one for our section
+      const filteredItems = [...menuItems].filter(elem => elem.children[0].getAttribute("href") === `#${id}`);
       filteredItems[0].classList.add("active");
     });
   }
 });
+
+
 
 /////////////// Highlight Menu Item END
