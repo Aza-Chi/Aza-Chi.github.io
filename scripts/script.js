8e7f795cbdf190e02c92359cd7cb0ba703fedc4a
Chi <azarashibobobo@gmail.com>
2024-05-04 22:44:29 +0100

    updated style.css index.html scripts.js

--- scripts/script.js
+++ scripts/script.js
@@ -46,77 +46,36 @@ document.onload = setThemePreference();
 ///////////////////
 /////////////// Highlight Menu Item WIP!!!
 
-const navList = document.getElementById("navbar__list");
-// const items = ["About", "Education", "Section 3", "Projects"];
-const items = ["About", "Education", "Projects"];
-const topMenu = document.getElementById("navbar__list");
-let lastId;
+// Get all sections that have an ID defined
+const sections = document.querySelectorAll("section[id]");
 
-//Build the nav
-items.forEach((item, i) => {
-  const el = document.createElement("a");
-  el.innerText = item;
-  el.classList.add("menu-items");
-  el.setAttribute("id", `menu-${i + 1}`);
-  el.href = `#section${i + 1}`;
-  //navList.appendChild(el);
+// Add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
 
-  const li = document.createElement("li");
-  li.classList.add("menu-list");
-  li.appendChild(el);
-  li.setAttribute("id", `${i + 1}`);
-  // Append the list item to the list
-  li.appendChild(el);
-  navList.appendChild(li);
-});
-
-//Make Nav Active when Clicked and scrolls down to section
-document.addEventListener("click", function(event) {
-  let active = document.querySelector(".menu-list.active");
-  if (active) active.classList.remove("active");
-  if (event.target.classList.contains("menu-list")) {
-    event.target.classList.add("active");
-    window.location.href = "#section" + event.target.id -500;
-  }
-});
-
-const topMenuHeight = topMenu.offsetTop - 100; //Zoom stuff for highlight nav
-const menuItems = document.querySelectorAll(".menu-list");
-const scrollItems = document.querySelectorAll("section");
-
-// Bind to scroll
-window.addEventListener("scroll", function() {
-  // Get container scroll position
-  const mainHeroHeight = document.querySelector(".main__hero").offsetTop;
-  console.log(mainHeroHeight);
-  let fromTop = window.pageYOffset + topMenuHeight + mainHeroHeight;
-  // Get id of current scroll item
-  let cur = [];
-
-  [...scrollItems].map(function(item) {
-    //debugger;
-    if (item.offsetTop < fromTop) {
-      cur.push(item);
+function navHighlighter() {
+  // Get current scroll position
+  let scrollY = window.pageYOffset;
+  
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+  
+    const sectionTop = (current.getBoundingClientRect().top + window.pageYOffset) - 50;
+    sectionId = current.getAttribute("id");
+    
+    /*
+    - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+    */
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight
+    ){
+      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.add("active");
+    } else {
+      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.remove("active");
     }
   });
-
-  // Get the id of the current element
-  //debugger;
-  cur = cur[cur.length - 1];
-  let id = cur ? cur.id : "";
-
-  if (lastId !== id) {
-    lastId = id;
-
-    menuItems.forEach(function(elem, index) {
-      elem.classList.remove("active");
-      // Look at the child "a" tags and try to find the one for our section
-      const filteredItems = [...menuItems].filter(elem => elem.children[0].getAttribute("href") === `#${id}`);
-      filteredItems[0].classList.add("active");
-    });
-  }
-});
-
-
+}
 
 /////////////// Highlight Menu Item END
